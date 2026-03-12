a8ceecba019b8e518406858fbbfaa04cdb90bb7b
Edvanderson <edvandersonsbabreu@gmail.com>
2026-03-12 15:02:10 -0300

    update

--- server.js
+++ server.js
@@ -1,20 +1,11 @@
 const express = require("express");
+
 const app = express();
 
-app.use(express.json());
-
-let tasks = [];
-
-app.get("/tasks", (req, res) => {
-  res.json(tasks);
-});
-
-app.post("/tasks", (req, res) => {
-  const task = { id: Date.now(), text: req.body.text };
-  tasks.push(task);
-  res.json(task);
+app.get("/", (req, res) => {
+  res.send("Projeto de devops");
 });
 
 app.listen(3000, () => {
-  console.log("API running on port 3000");
+  console.log("Servidor rodando na porta 3000");
 });
